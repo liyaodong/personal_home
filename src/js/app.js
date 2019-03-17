f0850dd7a34c0b45ff3058064e099744b34ea930
Yaodong Li <mail@liyaodong.com>
2019-03-18 00:01:04 +0800

    Add spa navitation

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,24 @@
 (() => {
+    const getSubsiteHandler = subsite => {
+        const subSiteList = [
+            {
+                match: name => name === 'blog',
+                handler: loadBlog,
+            },
+            {
+                match: name => /^posts/.test(name),
+                handler: loadPost,
+            },
+        ];
+
+        const handlerFounded = subSiteList.find(x => x.match(subsite));
+        if (handlerFounded) {
+            return handlerFounded.handler;
+        }
+
+        return null;
+    };
+
     const getSubsite = () => {
         const hash = window.location.hash.split('#')
         return hash[hash.length - 1] || 'home';
@@ -6,10 +26,62 @@
 
     const setSubsiteClass = subsite => document.body.setAttribute('data-subsite', subsite);
 
-    const updateSubsiteName = () => {
-        setSubsiteClass(getSubsite());
+    const updateSubsite = () => {
+        const subsite = getSubsite();
+        setSubsiteClass(subsite);
+
+        const handler = getSubsiteHandler(subsite);
+        if (typeof handler === 'function') {
+            handler(subsite);
+        }
     };
 
-    updateSubsiteName(); // for first page load
-    window.onhashchange = updateSubsiteName;
+    const loadBlog = () => {
+        const url = 'https://blog.liyaodong.com/';
+        fetch(url)
+            .then(res => res.text())
+            .then(parseBlogBody)
+            .then(updateBlogList);
+    };
+
+    const parseBlogBody = html => {
+        const el = document.createElement('html');
+        el.innerHTML = html;
+        return el.querySelectorAll('article.article-item');
+    };
+
+    const changeToHashUrl = a => {
+        const href = a.getAttribute('href');
+        a.setAttribute('href', href.replace(/^\//, '#'));
+    };
+
+    const updateBlogList = content => {
+        const $container = document.querySelector('.subsite--blog .subsite__body');
+        $container.innerHTML = '';
+
+        content.forEach(x => {
+            x.querySelectorAll('a').forEach(changeToHashUrl);
+            $container.append(x);
+        })
+    };
+
+    const loadPost = path => {
+        fetch(`https://blog.liyaodong.com/${path}`)
+            .then(res => res.text())
+            .then(html => {
+                const el = document.createElement('html');
+                el.innerHTML = html;
+                const title = el.querySelector('title').innerText;
+                document.title = title;
+                return el.querySelector('section.single');
+            })
+            .then(content => {
+                const $container = document.querySelector('.subsite--post .subsite__body');
+                $container.innerHTML = '';
+                $container.append(content);
+            });
+    };
+
+    updateSubsite(); // for first page load
+    window.onhashchange = updateSubsite;
 })();
